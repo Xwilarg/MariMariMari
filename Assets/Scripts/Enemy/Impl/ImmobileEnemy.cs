93da49e4f8d828bfa172084c0ad5c47270419420
Xwilarg <xwilarg@protonmail.com>
2024-06-15 22:44:10 +0200

    Clean code

--- Assets/Scripts/Enemy/Impl/ImmobileEnemy.cs
+++ Assets/Scripts/Enemy/Impl/ImmobileEnemy.cs
@@ -4,8 +4,6 @@ namespace TouhouPride.Enemy.Impl
 {
     public class ImmobileEnemy : AEnemyController
     {
-        private bool _canTakeDamage;
-
         protected override Vector2? DoesAttack() => AttackClosest();
 
         protected override Vector2 Move() => Vector2.zero;
