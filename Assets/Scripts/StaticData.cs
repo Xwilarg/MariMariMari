fe459e4f3205d6caa225655ad6bbb8f9390a0165
lydia <parker.verboom@gmail.com>
2024-06-17 01:58:06 -0300

    Removed defualt StaticData.CharacterEndSprite value

--- Assets/Scripts/StaticData.cs
+++ Assets/Scripts/StaticData.cs
@@ -5,8 +5,6 @@ namespace TouhouPride
     public static class StaticData
     {
         public static string CharacterName { set; get; } = "Reimu";
-
-        public static Sprite CharacterEndSprite { set; get; } = Resources.Load<Sprite>("Sprite Assets");
-        //public static Sprite CharacterEndSprite { set; get; }
+        public static Sprite CharacterEndSprite { set; get; }
     }
 }
