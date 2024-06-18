fa050ba763250b26f4acefdbc2633b71dbde9552
Xwilarg <xwilarg@protonmail.com>
2024-06-18 21:20:32 +0200

    Add base boss patterns

--- Assets/Scripts/Enemy/Impl/BossEnemy.cs
+++ Assets/Scripts/Enemy/Impl/BossEnemy.cs
@@ -1,9 +1,16 @@
+using TouhouPride.Manager;
 using UnityEngine;
 
 namespace TouhouPride.Enemy.Impl
 {
     public class BossEnemy : AEnemyController
     {
+        private BossStateType _state;
+        private float _timer = 2f;
+
+        private float _speed = 1f;
+        private Vector2 _direction = Vector2.zero;
+
         protected override void Awake()
         {
             base.Awake();
@@ -11,9 +18,42 @@ namespace TouhouPride.Enemy.Impl
             _canTakeDamage = false;
         }
 
-        protected override Vector2? DoesAttack() => AttackClosest();
+        private void Update()
+        {
+            if (!IsActive) return;
 
-        protected override Vector2 Move() => Vector2.zero;
+            _timer -= Time.deltaTime;
+            if (_timer <= 0f)
+            {
+                switch (_state)
+                {
+                    case BossStateType.Idle: // Rush toward player to attack
+                        _speed = 3f;
+                        _direction = PlayerManager.Instance.Player.transform.position - transform.position;
+                        _timer = 2f;
+                        break;
+
+                    case BossStateType.AttackSword:
+                        _speed = 0f;
+                        _direction = Vector2.zero;
+                        _timer = 5f;
+
+                        // TODO: Spawn bullets
+                        break;
+
+                    default:
+                        throw new System.NotImplementedException();
+                }
+                if (_state == BossStateType.AttackSword) _state = BossStateType.Idle;
+                else _state++;
+            }
+        }
+
+        protected override Vector2? DoesAttack() => null;
+
+        protected override Vector2 Move() => _direction;
+
+        protected override float MoveSpeed => _speed;
 
         public void AllowDamage()
         {
@@ -21,5 +61,11 @@ namespace TouhouPride.Enemy.Impl
         }
 
         protected override bool PlayMoveAnimations => false;
+
+        private enum BossStateType
+        {
+            Idle,
+            AttackSword
+        }
     }
 }
