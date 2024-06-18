4d9392c28a3befcf4990284b22ea32ec534f6f9f
Xwilarg <xwilarg@protonmail.com>
2024-06-18 21:38:43 +0200

    Add basic boss AI

--- Assets/Scripts/Enemy/Impl/BossEnemy.cs
+++ Assets/Scripts/Enemy/Impl/BossEnemy.cs
@@ -1,5 +1,7 @@
+using Projectiles;
 using TouhouPride.Manager;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 namespace TouhouPride.Enemy.Impl
 {
@@ -10,6 +12,8 @@ namespace TouhouPride.Enemy.Impl
 
         private float _speed = 1f;
         private Vector2 _direction = Vector2.zero;
+
+        private Vector2 _basePos;
 
         protected override void Awake()
         {
@@ -28,8 +32,9 @@ namespace TouhouPride.Enemy.Impl
                 switch (_state)
                 {
                     case BossStateType.Idle: // Rush toward player to attack
-                        _speed = 3f;
+                        _speed = 10f;
                         _direction = PlayerManager.Instance.Player.transform.position - transform.position;
+                        _basePos = transform.position;
                         _timer = 2f;
                         break;
 
@@ -37,6 +42,21 @@ namespace TouhouPride.Enemy.Impl
                         _speed = 0f;
                         _direction = Vector2.zero;
                         _timer = 5f;
+
+                        var p1 = transform.position;
+                        var p2 = _basePos;
+                        var mid = new Vector2((p1.x + p2.x) / 2f, (p1.y + p2.y) / 2f);
+                        var dir = (Vector2)transform.position - mid;
+
+                        var prefab = ResourcesManager.Instance.Bullet;
+                        var max = Vector2.Distance(p1, p2);
+                        for (float x = 0; x < max; x += .5f)
+                        {
+
+                            var go = Instantiate(prefab, Vector2.Lerp(p1, p2, x / max), Quaternion.identity);
+                            go.layer = LayerMask.NameToLayer("EnemyProjectile");
+                            go.GetComponent<StandardBullet>().Movement(dir);
+                        }
 
                         // TODO: Spawn bullets
                         break;
