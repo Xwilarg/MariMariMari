9e8eddae13e7bbd0683c305fb5139d3e869453f7
lydia <parker.verboom@gmail.com>
2024-06-20 13:55:35 -0300

    Left/Right enemy has vertical toggle

--- Assets/Scripts/Enemy/Impl/LeftRightEnemy.cs
+++ Assets/Scripts/Enemy/Impl/LeftRightEnemy.cs
@@ -5,7 +5,11 @@ namespace TouhouPride.Enemy.Impl
     public class LeftRightEnemy : AEnemyController
     {
         private bool _goLeft;
+        private bool _goDown;
+        
         private int _wallMask;
+
+        public bool verticalMovement = false;
 
         protected override void Awake()
         {
@@ -18,12 +22,24 @@ namespace TouhouPride.Enemy.Impl
 
         protected override Vector2 Move()
         {
-            var check = Physics2D.Raycast(transform.position, _goLeft ? Vector2.left : Vector2.right, 100f, _wallMask);
-            if (check.collider != null && check.distance < 1f)
+            if (verticalMovement)
             {
-                _goLeft = !_goLeft;
+                var check = Physics2D.Raycast(transform.position, _goDown ? Vector2.down : Vector2.up, 100f, _wallMask);
+                if (check.collider != null && check.distance < 1f)
+                {
+                    _goDown = !_goDown;
+                }
+                return Vector2.up * (_goDown ? -1 : 1);
             }
-            return Vector2.right * (_goLeft ? -1 : 1);
+            else
+            {
+                var check = Physics2D.Raycast(transform.position, _goLeft ? Vector2.left : Vector2.right, 100f, _wallMask);
+                if (check.collider != null && check.distance < 1f)
+                {
+                    _goLeft = !_goLeft;
+                }
+                return Vector2.right * (_goLeft ? -1 : 1);
+            }
         }
     }
 }
