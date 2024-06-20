4f2fc48303754018c97559e5a44ae14cbbe10bf2
lydia <parker.verboom@gmail.com>
2024-06-20 14:19:41 -0300

    Forgot to remove commented code oops

--- Assets/Scripts/Enemy/Impl/LeftRightEnemy.cs
+++ Assets/Scripts/Enemy/Impl/LeftRightEnemy.cs
@@ -4,15 +4,10 @@ namespace TouhouPride.Enemy.Impl
 {
     public class LeftRightEnemy : AEnemyController
     {
-        /*
-        private bool _goLeft;
-        private bool _goDown;
-        */
-
         private bool _goForward;
 
-        private Vector2 forwardDirection = Vector2.left;
-        private Vector2 backwardDirection = Vector2.right;
+        private Vector2 _forwardDirection = Vector2.left;
+        private Vector2 _backwardDirection = Vector2.right;
         
         private int _wallMask;
 
@@ -24,8 +19,8 @@ namespace TouhouPride.Enemy.Impl
 
             if (verticalMovement == true)
             {
-                forwardDirection = Vector2.up;
-                backwardDirection = Vector2.down;
+                _forwardDirection = Vector2.up;
+                _backwardDirection = Vector2.down;
             }
             
             _wallMask = LayerMask.GetMask("Wall");
@@ -35,33 +30,12 @@ namespace TouhouPride.Enemy.Impl
 
         protected override Vector2 Move()
         {
-            var check = Physics2D.Raycast(transform.position, _goForward ? forwardDirection : backwardDirection, 100f, _wallMask);
+            var check = Physics2D.Raycast(transform.position, _goForward ? _forwardDirection : _backwardDirection, 100f, _wallMask);
             if (check.collider != null && check.distance < 1f)
             {
                 _goForward = !_goForward;
             }
-            return backwardDirection * (_goForward ? -1 : 1);
-            
-            /*
-            if (verticalMovement)
-            {
-                var check = Physics2D.Raycast(transform.position, _goDown ? Vector2.down : Vector2.up, 100f, _wallMask);
-                if (check.collider != null && check.distance < 1f)
-                {
-                    _goDown = !_goDown;
-                }
-                return Vector2.up * (_goDown ? -1 : 1);
-            }
-            else
-            {
-                var check = Physics2D.Raycast(transform.position, _goLeft ? Vector2.left : Vector2.right, 100f, _wallMask);
-                if (check.collider != null && check.distance < 1f)
-                {
-                    _goLeft = !_goLeft;
-                }
-                return Vector2.right * (_goLeft ? -1 : 1);
-            }
-            */
+            return _backwardDirection * (_goForward ? -1 : 1);
         }
     }
 }
